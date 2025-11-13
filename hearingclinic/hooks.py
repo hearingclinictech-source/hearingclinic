718896df2d962d38800ca1af7d5a647f57b71514
Thomas Roch <thomas@dierochs.de>
2025-11-14 07:53:48 +0800

    Remove installer scripts and hooks to run install with backup

--- hearingclinic/hooks.py
+++ hearingclinic/hooks.py
@@ -5,8 +5,6 @@ app_description = "All customizations for the Hearing Clinic ERPNext Implementat
 app_email = "thomas@dierochs.de"
 app_license = "mit"
 
-after_migrate = "hearingclinic.setup.install.after_install"
-
 doctype_js = {
        "Customer": [
            "public/js/Customer/format_customer_id.js",
